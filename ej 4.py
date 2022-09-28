3e8bdf1b2e02d7e78205817795773ba3c45680dd
JuliaFernandezBermejo <113622863+JuliaFernandezBermejo@users.noreply.github.com>
2022-09-28 16:47:48 +0100

    Update ej 4.py

--- ej 4.py
+++ ej 4.py
@@ -11,13 +11,4 @@ print(ciudad == 'Castellón' or ciudad_comp == 'Castellón')
 #c)
 print(ciudad != 'Castellón' and ciudad_comp != 'Castellón')
 #d)
-print(ciudad_comp == 'Castellón' or ciudad_comp == 'Valencia' or ciudad_comp == 'Alicante')
-#e)
-ciudad_comp = 'Valencia'
-print(ciudad_comp != 'Castellón' and ciudad_comp != 'Valencia' and ciudad_comp != 'Alicante')
-print(not(ciudad_comp == 'Castellón' or ciudad_comp == 'Valencia' or ciudad_comp == 'Alicante'))
-ciudad_comp = 'Teruel'
-print(ciudad_comp != 'Castellón' and ciudad_comp != 'Valencia' and ciudad_comp != 'Alicante')
-print(not(ciudad_comp == 'Castellón' or ciudad_comp == 'Valencia' or ciudad_comp == 'Alicante'))
-#f)
-print((9<edad>20) == (9<edad_comp>20))
+
